18bc664691fdc759a0682f826617ef4e60bd21c4
Dima <dvkollegov@mail.ru>
2023-10-05 19:56:26 +0300

    Java pre-project. Практическая задача 1.1.5 (исправление ошибок)

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
     private final SessionFactory sessionFactory = Util.getSessionFactory();
+
     public UserDaoHibernateImpl() {
 
     }
